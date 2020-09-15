a374b1757c9a8d14caa5e9a8fc207ba279a2d8e9
TomRobette <robettetom@gmail.com>
2020-09-15 14:02:38 +0000

    SimplEduc-0.0.1

---
+++ config/routes.php
@@ -0,0 +1,26 @@
+<?php
+function getPage($db){
+    $lesPages['accueil']="accueilControleur";
+    
+    
+
+    /*if($db!=NULL){
+        if(isset($_GET['page'])) {
+            $page=$_GET['page'];
+        }else{
+            $page='accueil';
+        }
+    
+        if(isset($lesPages[$page])){
+            $contenu=$lesPages[$page];
+        
+        }else{
+            $contenu=$lesPages['accueil'];
+        }
+    
+        return $contenu;
+    }else{
+        return $lesPages['maintenance'];
+    }*/
+}
+?>
